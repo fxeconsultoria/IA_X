0a965818c78c59b870565746215c6e342426c4c5
fxeconsultoria <fxeconsultoria@gmail.com>
2025-07-05 15:01:26 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import App from './App.jsx'
-import DeliveryIA from './pages/DeliveryIA.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/delivery-ia" element={<DeliveryIA />} />
-      </Routes>
-    </Router>
-  </React.StrictMode>,
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
-
